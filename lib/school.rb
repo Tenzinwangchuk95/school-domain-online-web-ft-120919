dce4fd13a9c9f3f0818aa06f219a889dbca45c47
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-18 00:44:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,14 @@
 # code here!
 class School
   
-  ROSTER = {}
+  @roster = {}
   
   def initialize(name)
     @name = name
   end
   
   def roster
-    ROSTER 
+    @roster 
   end
   
   def add_student(name, grade)
